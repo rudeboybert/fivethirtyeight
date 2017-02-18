cc165b8dc9929372ab96f1b4c153d2fa7260ee99
rudeboybert <albert.ys.kim@gmail.com>
2017-02-18 17:46:49 -0500

    Added overall package help file and updated README

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,11 @@
 # fivethirtyeight 0.1.0.9000
 
 * Development version
-* [Removed explicit loading of data in vignette](https://github.com/rudeboybert/fivethirtyeight/pull/2)
-* [Changed to SVG travis logo](https://github.com/rudeboybert/fivethirtyeight/pull/1)
+* Added global help file for package: `?fivethirtyeight` with complete instructions
+* Other minor changes:
+    + [Changed to SVG travis logo](https://github.com/rudeboybert/fivethirtyeight/pull/1)
+    + [Removed explicit loading of data in vignette](https://github.com/rudeboybert/fivethirtyeight/pull/2)
+    + `fivethirtyeight` main vignette reads data set list from Google Sheets master document [https://goo.gl/OT8iHa](https://goo.gl/OT8iHa)
 
 # fivethirtyeight 0.1.0
 
---
+++ R/fivethirtyeight.R
@@ -0,0 +1,30 @@
+#' fivethirtyeight: Data and Code Behind the Stories and Interactives at 'FiveThirtyEight'
+#'
+#' An R library that provides access to the code and data sets
+#' published by FiveThirtyEight \url{https://github.com/fivethirtyeight/data}. Note
+#' that while we received guidance from editors at 538, this package is not
+#' officially published by 538. Contribute to this package at 
+#' \url{https://github.com/rudeboybert/fivethirtyeight}.
+#'
+#' @docType package
+#' @name fivethirtyeight
+#' @examples 
+#' # Example usage:
+#' library(fivethirtyeight)
+#' head(bechdel)
+#' 
+#' # All information about any data set can be found in the help file:
+#' ?bechdel
+#' 
+#' # To view a list of all data sets:
+#' data(package = "fivethirtyeight")
+#' 
+#' # To view a detailed list of all data sets:
+#' vignette("fivethirtyeight", package = "fivethirtyeight")
+#' 
+#' # Some data sets include vignettes with an example analysis:
+#' vignette("bechdel", package = "fivethirtyeight")
+#' 
+#' # To browse all vignettes:
+#' browseVignettes(package = "fivethirtyeight")
+NULL
