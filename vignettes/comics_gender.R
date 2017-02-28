6bcf643ccfe2ea0200bdd6b2e14b556dbfad84f6
Jonathan Bouchet <bouchetjonathan@gmail.com>
2017-02-27 20:43:54 -0500

    changes from the pull-request

--- vignettes/comics_gender.R
+++ vignettes/comics_gender.R
@@ -8,115 +8,125 @@ library(knitr)
 library(wordcloud)
 library(gridExtra)
 
-#calculate percentage of each gender for each publisher
 percent_gen_pub <- comic_characters %>% 
-  group_by(sex, publisher) %>% 
-  summarise(number = n()) %>% 
-  arrange(-number)
-percent_gen_pub <- percent_gen_pub %>% 
-  group_by(publisher) %>% 
-  mutate(countT = sum(number)) %>% 
-  group_by(sex) %>% 
-  mutate(percentage = 100*number/countT) %>% 
-  mutate(label = paste0(round(percentage, 2)))
-
-#plot percentage of each gender for each publisher
-percentage_per_publisher <- ggplot(data = percent_gen_pub,aes(x = sex,y = percentage, fill = publisher)) + 
-  geom_bar(width = 0.9, stat = "identity", position = 'dodge') 
+	group_by(sex, publisher) %>% 
+	summarise(number = n()) %>% 
+	arrange(-number) %>%
+	group_by(publisher) %>% 
+	mutate(countT = sum(number)) %>% 
+	group_by(sex) %>% 
+	mutate(
+		percentage = (100*number/countT),
+		label = paste0(round(percentage, 2)))
   
+#plot percentage of each gender for each publisher
+ggplot(data = percent_gen_pub,aes(x = sex,y = percentage, fill = publisher)) + 
+	geom_bar(width = 0.9, stat = "identity", position = 'dodge') +
+	scale_fill_manual(values = c("#3B9AB2","#EBCC2A"))
+
+
 #plot raw number of each gender for each publisher
 raw_number_per_publisher <- comic_characters %>% 
-  group_by(publisher) %>% 
-  summarise(number = n()) %>% 
-  arrange(-number) %>% 
-  ggplot(aes(x = publisher, y = number, fill = publisher)) + 
-  geom_bar(width = 0.1, stat = "identity") + 
-  coord_flip()
+	group_by(publisher) %>% 
+	summarise(number = n()) %>% 
+	arrange(-number)
   
+ggplot(data = raw_number_per_publisher, aes(x = publisher, y = number, fill = publisher)) + 
+	geom_bar(width = 0.1, stat = "identity") + 
+	coord_flip() +
+	scale_fill_manual(values = c("#3B9AB2","#EBCC2A"))
+
+
 #select data with no NA's for sex and date and groupby
-marvel_vs_time <- as.data.frame(comic_characters %>% 
-                                filter(publisher == 'Marvel' & !is.na(month) & !is.na(sex)) %>%
-                                group_by(year, month, sex) %>% summarise(number = n()))
+#define list of gender per publisher
+gender_list_marvel <- c("Female Characters", "Male Characters", "Genderfluid Characters","Agender Characters")
+gender_list_dc <- c("Female Characters", "Male Characters", "Genderless Characters","Transgender Characters")
 
-marvel_vs_time$ordered_month <- factor(marvel_vs_time$month, levels = month.name)
-marvel_vs_time$sex <- factor(marvel_vs_time$sex, levels = c("Female Characters", "Male Characters", "Genderfluid Characters","Agender Characters"))
-g1<-ggplot(data = marvel_vs_time, aes(year, ordered_month)) + geom_tile(aes(fill = number),colour = "white") + 
-  scale_fill_gradient(low = "#EBCC2A", high = "black") + 
-  facet_wrap(~ sex, ncol = 4)
-  
-dc_vs_time <- as.data.frame(comic_characters %>% filter(publisher == 'DC' & !is.na(month) & !is.na(sex)))
-dc_vs_time$month <- ifelse(dc_vs_time$month == "01","January",dc_vs_time$month)
-dc_vs_time <- dc_vs_time %>% filter(month != "Holiday") %>% group_by(year,month,sex) %>% summarise(number = n())
-dc_vs_time$month <- as.factor(dc_vs_time$month)
-dc_vs_time$ordered_month <- factor(dc_vs_time$month, levels = month.name)
-dc_vs_time$sex <- factor(dc_vs_time$sex, levels = c("Female Characters", "Male Characters", "Genderless Characters","Transgender Characters"))
+marvel_vs_time <- comic_characters %>% 
+	filter(publisher == 'Marvel' & !is.na(month) & !is.na(sex)) %>% 
+	group_by(year, month, sex) %>% 
+	summarise(number = n()) %>% 
+	mutate(
+		sex_ordered = factor(sex, levels = gender_list_marvel), 
+		month_ordered = factor(month, levels = month.name))
 
-g2 <- ggplot(data = dc_vs_time, aes(year, ordered_month)) + geom_tile(aes(fill = number),colour = "white") + 
-  scale_fill_gradient(low = "#3B9AB2", high = "black") + 
-  facet_wrap(~ sex, ncol = 4)
-  
+ggplot(data = marvel_vs_time, aes(year, month_ordered)) + 
+	geom_tile(aes(fill = number),colour = "white") + 
+	scale_fill_gradient(low = "#EBCC2A", high = "black") + 
+	facet_wrap(~ sex_ordered, ncol = 4) 
+
+dc_vs_time <- comic_characters %>% 
+	filter(publisher == 'DC' & month!= "Holiday" & !is.na(month) & !is.na(sex)) %>% 
+		mutate(month = ifelse(month=="01","January",month)) %>% 
+		group_by(year, month, sex) %>% summarise(number = n()) %>% 
+		mutate(
+			sex_ordered = factor(sex, levels = gender_list_dc), 
+			month_ordered = factor(month, levels = month.name)) 
+
+ggplot(data = dc_vs_time, aes(year, month_ordered)) + 
+	geom_tile(aes(fill = number),colour = "white") + 
+	scale_fill_gradient(low = "#3B9AB2", high = "black") + 
+	facet_wrap(~ sex_ordered, ncol = 4)
+
 #function to aggregate data by (gender, feature, publisher)
 aggregateFeature <- function(current_publisher, feature){
-  #empty list to keep dataframe by (gender, feature)
-  currentFeature <- list()
-  if(current_publisher == 'Marvel'){
-    gender_list <- c("Male Characters","Female Characters", "Genderfluid Characters","Agender Characters")
-  } else {
-  gender_list <- c("Male Characters","Female Characters", "Genderfluid Characters","Agender Characters")
-  }
-
-for(i in 1:length(gender_list)){
-	currentFeature[[i]] <- as.data.frame(comic_characters %>% 
-	                                filter(publisher == current_publisher ) %>%  
-	                                select(hair, sex) %>% 
-	                                na.omit() %>% 
-	                                group_by(hair) %>% 
-	                                filter(sex == gender_list[i]) %>% 
-	                                summarise(number = n()) %>% 
-	                                arrange(-number) %>% 
-	                                mutate(countT = sum(number)) %>% 
-	                                mutate(percentage = round(100*number/countT,1)) %>% 
-	                                select(hair, percentage, number))
+	#empty list to keep dataframe by (gender, feature)
+	currentFeature <- list()
 	if(current_publisher == 'Marvel'){
-	  colnames(currentFeature[[i]])[2]<-'percentage_marvel'
-	  } else{
-	    colnames(currentFeature[[i]])[2]<-'percentage_dc'
-	  }
+		gender_list <- gender_list_marvel
+		} else {
+			gender_list <- gender_list_dc
+		}
+
+	for(i in 1:length(gender_list)){
+		currentFeature[[i]] <- comic_characters %>% 
+			filter(publisher == current_publisher ) %>%  
+			select(hair, sex) %>% 
+			na.omit() %>% 
+			group_by(hair) %>% 
+			filter(sex == gender_list[i]) %>% 
+			summarise(number = n()) %>% 
+			arrange(-number) %>% 
+			mutate(countT = sum(number)) %>% 
+			mutate(percentage = round(100*number/countT,1)) %>%
+			select(hair, percentage, number)
+		
+	if(current_publisher == 'Marvel'){
+		colnames(currentFeature[[i]])[2]<-'percentage_marvel'
+		} else{
+			colnames(currentFeature[[i]])[2]<-'percentage_dc'
+		}
 	
 	 if(feature == 'Hair'){
-	  #strip 'hair' word for better display of the radarchart
-	  currentFeature[[i]]$hair <- sapply(currentFeature[[i]]$hair, function(x) gsub(' Hair','', x))}
-	}
+	 	#strip 'hair' word for better display of the radarchart
+	 	currentFeature[[i]]$hair <- sapply(currentFeature[[i]]$hair, function(x) gsub(' Hair','', x))}
+	 	}
   
-  names(currentFeature) <- gender_list
-  return(currentFeature)
+	names(currentFeature) <- gender_list
+  	return(currentFeature)
 }
 
-#Radarchart plots
-#outer join the 2 dataframes for (feature=Hair, gender=Male)
-merged <- merge(aggregateFeature('DC','hair')[[1]], aggregateFeature('Marvel','hair')[[1]], by='hair', all=TRUE)
+merged <- full_join(aggregateFeature('DC','hair')[[1]], aggregateFeature('Marvel','hair')[[1]], by='hair')
 #set min/max percentages for the radarchart limits
 min <- rep(0, length(merged$hair))
 max <- rep(50, length(merged$hair))
-
 maleHair <- data.frame(rbind(max,min,merged$percentage_dc,merged$percentage_marvel))
 colnames(maleHair) <- merged$hair
 row.names(maleHair) <- c('max','min','percentage_dc','percentage_marvel')
 maleHair[is.na(maleHair)] <- 0
-radarchart(maleHair)  
+radarchart(maleHair)
 
 #Wordcloud plots
 #remove NA from appearance column, keep only the name inside parentheses
 marvel_appearances <- comic_characters %>% 
-  filter(publisher == 'Marvel') %>% 
-  select(name, appearances) %>% na.omit() %>% 
-  mutate(newname = gsub(" *\\(.*?\\) *", "", name))
+	filter(publisher == 'Marvel') %>% 
+	select(name, appearances) %>% na.omit() %>% 
+	mutate(name = gsub(" *\\(.*?\\) *", "", name))
+	
 dc_appearances <- comic_characters %>% 
-  filter(publisher == 'DC') %>% 
-  select(name, appearances) %>% na.omit() %>% 
-  mutate(newname = gsub(" *\\(.*?\\) *", "", name))
+	filter(publisher == 'DC') %>% 
+	select(name, appearances) %>% na.omit() %>% 
+	mutate(name = gsub(" *\\(.*?\\) *", "", name))
 
-wordcloud(dc_appearances$newname, dc_appearances$appearances)
-wordcloud(marvel_appearances$newname, marvel_appearances$appearances)
-  
-
+wordcloud(dc_appearances$name, dc_appearances$appearances)
+wordcloud(marvel_appearances$name, marvel_appearances$appearances)
