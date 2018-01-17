8a2f2a55bf50b59a72496ed40f4eb5e82cc1d464
Meredith Manley <mmanley18@amherst.edu>
2018-01-17 10:24:50 -0500

    second pass through

--- data-raw/process_data_sets_meredith.R
+++ data-raw/process_data_sets_meredith.R
@@ -16,33 +16,13 @@ devtools::use_data(ahca_polls, overwrite = TRUE)
 
 # bachelorette ---------------------------------------------------------------------
 bachelorette <- read_csv("data-raw/bachelorette/bachelorette.csv") %>%
-  clean_names() %>%
-  mutate(
-    season = as.factor(season),
-    elimination_1 = as.factor(elimination_1),
-    elimination_2 = as.factor(elimination_2),
-    elimination_3 = as.factor(elimination_3),
-    elimination_4 = as.factor(elimination_4),
-    elimination_5 = as.factor(elimination_5),
-    elimination_6 = as.factor(elimination_6),
-    elimination_7 = as.factor(elimination_7),
-    elimination_8 = as.factor(elimination_8),
-    elimination_9 = as.factor(elimination_9),
-    elimination_10 = as.factor(elimination_10),
-    dates_1 = as.factor(dates_1),
-    dates_2 = as.factor(dates_2),
-    dates_3 = as.factor(dates_3),
-    dates_4 = as.factor(dates_4),
-    dates_5 = as.factor(dates_5),
-    dates_6 = as.factor(dates_6),
-    dates_7 = as.factor(dates_7),
-    dates_8 = as.factor(dates_8),
-    dates_9 = as.factor(dates_9),
-    dates_10 = as.factor(dates_10)
-  ) %>%
-  filter(
-    season != "SEASON"
-  )
+  clean_names() %>% 
+  mutate_at(vars(starts_with("elimination")), as.factor) %>%
+  mutate_at(vars(starts_with("elimination")), funs(ifelse(. == "<NA>", NA, .))) %>%
+  mutate_at(vars(starts_with("dates")), as.factor) %>%
+  mutate_at(vars(starts_with("dates")), funs(ifelse(. == "<NA>", NA, .))) %>% 
+  mutate(season = as.integer(season)) %>%
+  filter(season != "SEASON")
 devtools::use_data(bachelorette, overwrite = TRUE)
 
 
@@ -59,7 +39,9 @@ candy_rankings <- read_csv("data-raw/candy-power-ranking/candy-data.csv") %>%
     hard = as.logical(hard),
     bar = as.logical(bar),
     pluribus = as.logical(pluribus)
-  )
+  ) %>%
+  mutate_at(competitorname, funs(gsub("Õ", "'", .))) ###########################
+
 devtools::use_data(candy_rankings, overwrite = TRUE)
 
 
@@ -76,7 +58,7 @@ devtools::use_data(chess_transfers, overwrite = TRUE)
 # generic_polllist
 generic_polllist <- 
   read_csv("https://projects.fivethirtyeight.com/generic-ballot-data/generic_polllist.csv")
-write.csv(generic_polllist, "generic_polllist.csv")
+write_csv(generic_polllist, path = "data-raw/congress-generic-ballot/generic_polllist.csv")
 
 generic_polllist <- 
   read_csv("data-raw/congress-generic-ballot/generic_polllist.csv") %>%
@@ -99,13 +81,13 @@ devtools::use_data(generic_polllist, overwrite = TRUE)
 # generic_topline
 generic_topline <- 
   read_csv("https://projects.fivethirtyeight.com/generic-ballot-data/generic_topline.csv")
-write.csv(generic_topline, "generic_topline.csv")
+write_csv(generic_topline, path = "data-raw/congress-generic-ballot/generic_topline.csv")
 
 generic_topline <- 
   read_csv("data-raw/congress-generic-ballot/generic_topline.csv") %>%
   clean_names() %>%
   mutate(
-    model_date = as.Date(model_date, "%m/%d/%Y"),
+    modeldate = as.Date(modeldate, "%m/%d/%Y"),
     timestamp = parse_date_time(timestamp, "HMS dmY"),
     subgroup = as.factor(subgroup)
   )
@@ -114,9 +96,7 @@ devtools::use_data(generic_topline, overwrite = TRUE)
 
 #inconvenient-sequel ----------------------------------------------------------------
 ratings <- read_csv("data-raw/inconvenient-sequel/ratings.csv") %>%
-  mutate(
-    category = as.factor(category)
-  )
+  mutate(category = as.factor(category))
 devtools::use_data(ratings, overwrite = TRUE)
 
 
@@ -132,7 +112,7 @@ devtools::use_data(tweets, overwrite = TRUE)
 
 # mlb-elo ---------------------------------------------------------------------------
 mlb_elo <- read_csv("https://projects.fivethirtyeight.com/mlb-api/mlb_elo.csv")
-write.csv(mlb_elo, "mlb_elo.csv")
+write_csv(mlb_elo, path = "data-raw/mlb-elo/mlb_elo.csv")
 
 mlb_elo <- read_csv("data-raw/mlb-elo/mlb_elo.csv") %>%
   mutate(
@@ -145,7 +125,7 @@ devtools::use_data(mlb_elo, overwrite = TRUE)
 # soccer-spi ------------------------------------------------------------------------
 #spi_matches
 spi_matches <- read_csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv")
-write.csv(spi_matches, "spi_matches.csv")
+write_csv(spi_matches, path = "data-raw/soccer-spi/spi_matches.csv")
 
 spi_matches <- read_csv("data-raw/soccer-spi/spi_matches.csv") %>%
   mutate(
@@ -156,7 +136,7 @@ devtools::use_data(spi_matches, overwrite = TRUE)
 #spi_global_rankings
 spi_global_rankings <- 
   read_csv("https://projects.fivethirtyeight.com/soccer-api/club/spi_global_rankings.csv")
-write.csv(spi_global_rankings, "spi_global_rankings.csv")
+write_csv(spi_global_rankings, path = "data-raw/soccer-spi/spi_global_rankings.csv")
 # No changes made
 devtools::use_data(spi_global_rankings, overwrite = TRUE)
 
