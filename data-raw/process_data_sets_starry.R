19634a202af9166678d57b1c7f40a3642a8935c8
ZHOUYUJIAA@GMAIL.COM <shinichiran>
2019-05-26 22:40:19 -0400

    preliminary data wrangling

--- data-raw/process_data_sets_starry.R
+++ data-raw/process_data_sets_starry.R
@@ -6,7 +6,27 @@ library(lubridate)
 
 # cabinet-turnover ---------------------------------------------------------------
 cabinet_turnover <- read_csv("data-raw/cabinet-turnover/cabinet-turnover.csv")
+colnames(cabinet_turnover) <- colnames(cabinet_turnover) %>% 
+  tolower() %>% 
+  str_replace_all(" ", "_")
+
+cabinet_turnover$start <- 
+  cabinet_turnover$start %>% 
+  as.Date("%m/%d/%y")
+cabinet_turnover$end <- 
+  cabinet_turnover$end %>% 
+  as.Date("%m/%d/%y")
+cabinet_turnover$president <- 
+  cabinet_turnover$president %>% 
+  as.factor()
+cabinet_turnover$position <- 
+  cabinet_turnover$position %>% 
+  as.factor()
+cabinet_turnover$length <- 
+  cabinet_turnover$length %>% 
+  as.numeric()
+cabinet_turnover$days <- 
+  cabinet_turnover$days %>% 
+  as.numeric()
 
 devtools::use_data(cabinet_turnover, overwrite = TRUE)
-
-# similar steps for cabinit-turnover, polish & "tame"
