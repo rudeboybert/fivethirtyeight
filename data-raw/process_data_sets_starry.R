086ac0779adfc92660b95d61fdcd9bce46258c5c
ZHOUYUJIAA@GMAIL.COM <shinichiran>
2019-05-27 12:53:15 -0400

    wrangling for august data

--- data-raw/process_data_sets_starry.R
+++ data-raw/process_data_sets_starry.R
@@ -8,24 +8,12 @@ colnames(cabinet_turnover) <- colnames(cabinet_turnover) %>%
   tolower() %>% 
   str_replace_all(" ", "_")
 
-cabinet_turnover$start <- 
-  cabinet_turnover$start %>% 
-  as.Date("%m/%d/%y")
-cabinet_turnover$end <- 
-  cabinet_turnover$end %>% 
-  as.Date("%m/%d/%y")
-cabinet_turnover$president <- 
-  cabinet_turnover$president %>% 
-  as.factor()
-cabinet_turnover$position <- 
-  cabinet_turnover$position %>% 
-  as.factor()
-cabinet_turnover$length <- 
-  cabinet_turnover$length %>% 
-  as.numeric()
-cabinet_turnover$days <- 
-  cabinet_turnover$days %>% 
-  as.numeric()
+cabinet_turnover$start <- as.Date(cabinet_turnover$start, "%m/%d/%y")
+cabinet_turnover$end <- as.Date(cabinet_turnover$end, "%m/%d/%y")
+cabinet_turnover$president <- as.factor(cabinet_turnover$president)
+cabinet_turnover$position <- as.factor(cabinet_turnover$position)
+cabinet_turnover$length <- as.numeric(cabinet_turnover$length)
+cabinet_turnover$days <- as.numeric(cabinet_turnover$days)
 
 devtools::use_data(cabinet_turnover, overwrite = TRUE)
 
@@ -35,5 +23,10 @@ colnames(august_senate_polls) <- colnames(august_senate_polls) %>%
   tolower() %>% 
   str_replace_all(" ", "_")
 
+august_senate_polls$cycle <- as.numeric(august_senate_polls$cycle)
+august_senate_polls$state <- as.factor(august_senate_polls$state)
+august_senate_polls$senate_class <- as.factor(august_senate_polls$senate_class)
+august_senate_polls$start_date <- as.Date(august_senate_polls$start_date, "%y/%m/%d") # need to transform order
+august_senate_polls$end_date <- as.Date(august_senate_polls$end_date, "%y/%m/%d")
 
 devtools::use_data(cabinet_turnover, overwrite = TRUE)
