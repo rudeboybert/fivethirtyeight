fa627b1dd9c89d15a51e735e163f9c165480b27c
ZHOUYUJIAA@GMAIL.COM <shinichiran>
2019-05-30 01:07:16 -0400

    add endorsement dataset, convert class of 2 datasets, tbd: finish documentation

--- R/data_starry.R
+++ R/data_starry.R
@@ -16,3 +16,52 @@
 #' }
 #' @source Federal Government Website, News Reports
 "cabinet_turnover"
+
+#' How Much Trouble Is Ted Cruz Really In?
+#'
+#' The raw data behind the story
+#' "How Much Trouble Is Ted Cruz Really In?"
+#' \url{https://fivethirtyeight.com/features/how-much-trouble-is-ted-cruz-really-in/}.
+#'
+#' @format A data frame with 594 rows representing "Senate polls that were conducted in August of an election year", and 11 variables (We weeded out any polls of races that didn’t have at least one Democrat and one Republican facing off.):
+#' \describe{
+#'   \item{cycle}{the election year}
+#'   \item{state}{the state of the poll}
+#'   \item{senate_class}{the class of the senate}
+#'   \item{start_date}{the start date of the poll}
+#'   \item{end_date}{the end odate of the poll}
+#'   \item{DEM_poll}{x}
+#'   \item{REP_poll}{x}
+#'   \item{DEM_result}{x}
+#'   \item{REP_result}{x}
+#'   \item{error}{x}
+#'   \item{absolute_error}{x}
+#'   }
+#' @source Emerson College’s poll of registered voters \url{https://www.emerson.edu/sites/default/files/Files/Academics/ecp-tx-aug2018-pr.pdf}
+"august_senate_polls"
+
+#' The 2020 Endorsement Primary - Which Democratic candidates are receiving the most support from prominent members of their party?
+#'
+#' The raw data behind the story
+#' "The 2020 Endorsement Primary - Which Democratic candidates are receiving the most support from prominent members of their party?"
+#' \url{https://projects.fivethirtyeight.com/2020-endorsements/democratic-primary/}.
+#'
+#' @format A data frame with 1000 rows representing X and 13 variables: 
+#' \describe{
+#'   \item{X}{Y}
+#'   }
+#' @source 
+"endorsements_2020"
+
+#' How FiveThirtyEight's 2018 Midterm Forecasts Did
+#' 
+#' The raw data behind the story 
+#' 'How FiveThirtyEight's 2018 Midterm Forecasts Did'
+#' \url {https://fivethirtyeight.com/features/how-fivethirtyeights-2018-midterm-forecasts-did/}
+#' 
+#' @format A dataframe with 1518 rows and 11 variables:
+#' \describe{
+#'   \item{X}{Y}
+#'   }
+#' @source 
+"forecast_results_2018"
--- data-raw/process_data_sets_starry.R
+++ data-raw/process_data_sets_starry.R
@@ -20,6 +20,9 @@ devtools::use_data(cabinet_turnover, overwrite = TRUE)
 
 # august-senate-polls ---------------------------------------------------------------
 august_senate_polls <- read_csv("data-raw/august-senate-polls/august_senate_polls.csv")
+colnames(august_senate_polls) <- colnames(august_senate_polls) %>% 
+  tolower() %>% 
+  str_replace_all(" ", "_")
 august_senate_polls <- august_senate_polls %>% 
   mutate(
     cycle = as.numeric(cycle), 
@@ -28,4 +31,18 @@ august_senate_polls <- august_senate_polls %>%
     start_date = as.Date(start_date), 
     end_date = as.Date(end_date))
 
-devtools::use_data(cabinet_turnover, overwrite = TRUE)
+devtools::use_data(august_senate_polls, overwrite = TRUE)
+
+# endorsements ---------------------------------------------------------------
+endorsements_2020 <- read_csv("data-raw/endorsements/endorsements-2020.csv")
+
+
+devtools::use_data(endorsements_2020, overwrite = TRUE)
+
+
+# forecast-review ---------------------------------------------------------------
+forecast_results_2018 <- read_csv("data-raw/forecast-review/forecast_results_2018.csv")
+
+
+devtools::use_data(forecast_results_2018, overwrite = TRUE)
+
