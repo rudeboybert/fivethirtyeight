9242b857b22261859ff95e15dd40bdc5402c44a6
Elaineyex <xye@smith.edu>
2019-07-30 03:37:23 +0800

    test package_elaine

---
+++ R/data_elaine.R
@@ -0,0 +1,15 @@
+#' How Many Bad Drivers Are There in Different States?
+#'
+#' The raw data behind the story
+#' "How Much Trouble Is Ted Cruz Really In?"
+#' \url{https://fivethirtyeight.com/features/how-much-trouble-is-ted-cruz-really-in/}.
+#'
+#' @format A data frame with 5 rows representing senate polls, and 4 variables:
+#' \describe{
+#'   \item{state}{the state}
+#'   \item{number_of_drivers_involved_in_fatal_collisions_per_million_miles}{the number of drivers involved in fatal collisions per billion miles}
+#'   \item{percentage_of_drivers_involved_in_fatal_collisions_who_were_speeding}{the percentage Of Drivers Involved In Fatal Collisions Who Were Speeding}
+#'   \item{percentage_of_drivers_involved_in_fatal_collisions_who_were_alcohol_impaired}{the percentage Of Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired}
+#'   }
+#' @source Emerson College’s poll of registered voters \url{https://www.emerson.edu/sites/default/files/Files/Academics/ecp-tx-aug2018-pr.pdf}
+"bad_drivers_2"
