7376e5241938c1f98d5ec4d7c6ae6d17ce253c74
Natalia Iannucci <niannucci@smith.edu>
2019-11-05 16:40:09 -0500

    put documentation for both datasets into one man file

--- R/data_natalia.R
+++ R/data_natalia.R
@@ -1,10 +1,19 @@
-#' Beto O'Rourke Ignored Cable News - And It Ignored Him
-#'
-#' The raw data behind the story
+#' @name media_mentions
+#' @rdname media_mentions
+#' 
+#' @title 2020 Presidential Candidates Media Mentions
+#' 
+#' @description The raw data behind the story
 #' "Beto O'Rourke Ignored Cable News - And It Ignored Him"
 #' \url{https://fivethirtyeight.com/features/beto-orourke-ignored-cable-news-and-it-ignored-him/}
+#' 
+NULL
+
+#' @rdname media_mentions
 #'
-#' @format A data frame with 972 rows representing weeks of cable coverage and 7 variables:
+#' @format 2 dataframes about 2020 presidential candidate media mentions
+#' @section media_mentions_cable:
+#' A data frame with 972 rows representing weeks of cable coverage and 7 variables:
 #' \describe{
 #'   \item{date}{start date for the week of coverage}
 #'   \item{name}{candidate's name}
@@ -14,17 +23,16 @@
 #'   \item{pct_of_all_candidate_clips}{percentage of clips in which that specific candidate is mentioned out of all clips mentioning any candidate for that week (matched_clips / all_candidate_clips)}
 #'   \item{query}{query used for the GDELT Television API}
 #' }
+#' @export
 #' @source The GDELT Television API \url{https://blog.gdeltproject.org/gdelt-2-0-television-api-debuts/}, which processes the data from the TV News Archive \url{https://archive.org/details/tv}.
+#' @export
 "media_mentions_cable"
 
-
-#' Beto O'Rourke Ignored Cable News - And It Ignored Him
+#' @rdname media_mentions
 #'
-#' The raw data behind the story
-#' "Beto O'Rourke Ignored Cable News - And It Ignored Him"
-#' \url{https://fivethirtyeight.com/features/beto-orourke-ignored-cable-news-and-it-ignored-him/}
-#'
-#' @format A data frame with 954 rows representing weeks and 6 variables:
+#' @format
+#' @section media_mentions_online:  
+#' A data frame with 954 rows representing weeks and 6 variables:
 #' \describe{
 #'   \item{date}{start date for the week of coverage}
 #'   \item{name}{candidate's name}
@@ -32,6 +40,8 @@
 #'   \item{all_candidate_stories}{number of stories in that week that mention any candidate}
 #'   \item{pct_of_all_candidate_stories}{percentage of stories in which that specific candidate is mentioned out of all stories mentioning any candidate for that week (matched_stories / all_candidate_stories)}
 #'   \item{query}{query for Media Cloud}
-#' }
-#'@source Two collections in the Media Cloud \url{https://mediacloud.org/} database U.S. Top Online News \url{https://sources.mediacloud.org/#/collections/58722749} and U.S. Top Digital Native News \url{https://sources.mediacloud.org/#/collections/57078150}
+#'}
+#' @export
+#' @source Two collections in the Media Cloud \url{https://mediacloud.org/} database U.S. Top Online News \url{https://sources.mediacloud.org/#/collections/58722749} and U.S. Top Digital Native News \url{https://sources.mediacloud.org/#/collections/57078150}
+#' @export
 "media_mentions_online"
