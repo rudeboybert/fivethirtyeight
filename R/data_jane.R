ebe82e7b4f8ce1e075e0d1752fd42af1e42dd9e2
Jane Bang <jbang19@smith.edu>
2019-11-18 17:10:03 -0500

    Fixed nba_draymond

--- R/data_jane.R
+++ R/data_jane.R
@@ -8,8 +8,8 @@
 #' \describe{
 #'   \item{season}{The second year of the season; for example, 2018-2019 season would be listed as 2019}
 #'   \item{player}{Name of the player}
-#'   \item{posessions}{Number of possessions a player during the season}
-#'   \item{DRAYMOND}{Defensive Rating Accounting for Yielding Minimal Openness by Nearest Defender}
+#'   \item{possessions}{Number of possessions a player during the season}
+#'   \item{draymond}{Defensive Rating Accounting for Yielding Minimal Openness by Nearest Defender}
 #'    }
 #' @source NBA
 "nba_draymond"
