0f07a3de24ae84e47af4818b27f942e6546ed8a6
danicamiguel <dmiguel@smith.edu>
2019-11-19 11:50:09 -0500

    Made "era" variable the first variable and made all the variables the same type and in the same order

--- data-raw/process_data_set_danica.R
+++ data-raw/process_data_set_danica.R
@@ -11,16 +11,22 @@ historical_raptor_by_player <- read_csv("data-raw/nba-raptor/historical_RAPTOR_b
   ) 
 historical_raptor_by_player[c("raptor_box_offense", "raptor_box_defense","raptor_box_total", 
             "raptor_onoff_offense", "raptor_onoff_defense", "raptor_onoff_total")] <- NA
-historical_raptor_by_player <- historical_raptor_by_player[c("player_name","player_id","season","poss","mp","raptor_box_offense","raptor_box_defense","raptor_box_total",    
+historical_raptor_by_player <- historical_raptor_by_player[c("era","player_name","player_id","season","poss","mp","raptor_box_offense","raptor_box_defense","raptor_box_total",    
                               "raptor_onoff_offense","raptor_onoff_defense","raptor_onoff_total","raptor_offense","raptor_defense","raptor_total",
                               "war_total","war_reg_season","war_playoffs","predator_offense","predator_defense","predator_total",
-                              "pace_impact","era")]
+                              "pace_impact")] %>%
+  mutate_all(as.character)
 
 #Modern Raptor by Player
 modern_raptor_by_player <- read_csv("data-raw/nba-raptor/modern_RAPTOR_by_player.csv") %>%
   mutate(
     era = factor(c("modern"))
-  )
+  ) 
+modern_raptor_by_player <- modern_raptor_by_player[c("era","player_name","player_id","season","poss","mp","raptor_box_offense","raptor_box_defense","raptor_box_total",    
+                                                             "raptor_onoff_offense","raptor_onoff_defense","raptor_onoff_total","raptor_offense","raptor_defense","raptor_total",
+                                                             "war_total","war_reg_season","war_playoffs","predator_offense","predator_defense","predator_total",
+                                                             "pace_impact")] %>%
+  mutate_all(as.character)
 
 #Historical Raptor by Team
 historical_raptor_by_team <-read_csv("data-raw/nba-raptor/historical_RAPTOR_by_team.csv") %>%
@@ -29,18 +35,24 @@ historical_raptor_by_team <-read_csv("data-raw/nba-raptor/historical_RAPTOR_by_t
     era = factor(c("historical"))
     )
 historical_raptor_by_team[c("raptor_box_offense", "raptor_box_defense","raptor_box_total", 
-                                                             "raptor_onoff_offense", "raptor_onoff_defense", "raptor_onoff_total")] <- NA
-historical_raptor_by_team <- historical_raptor_by_team[c("player_name","player_id","season","season_type","team","poss","mp","raptor_box_offense","raptor_box_defense","raptor_box_total",    
+            "raptor_onoff_offense", "raptor_onoff_defense", "raptor_onoff_total")] <- NA
+historical_raptor_by_team <- historical_raptor_by_team[c("era","player_name","player_id","season","season_type","team","poss","mp","raptor_box_offense","raptor_box_defense","raptor_box_total",    
                                                              "raptor_onoff_offense","raptor_onoff_defense","raptor_onoff_total","raptor_offense","raptor_defense","raptor_total",
                                                              "war_total","war_reg_season","war_playoffs","predator_offense","predator_defense","predator_total",
-                                                             "pace_impact","era")]
+                                                             "pace_impact")] %>%
+  mutate_all(as.character)
 
 #Modern Raptor by Team
 modern_raptor_by_team <-read_csv("data-raw/nba-raptor/modern_RAPTOR_by_team.csv") %>%
   mutate(
     season_type = as.factor(season_type),
     era = factor(c("modern"))
-  )
+  ) 
+modern_raptor_by_team <- modern_raptor_by_team[c("era","player_name","player_id","season","season_type","team","poss","mp","raptor_box_offense","raptor_box_defense","raptor_box_total",    
+                                                         "raptor_onoff_offense","raptor_onoff_defense","raptor_onoff_total","raptor_offense","raptor_defense","raptor_total",
+                                                         "war_total","war_reg_season","war_playoffs","predator_offense","predator_defense","predator_total",
+                                                         "pace_impact")] %>%
+  mutate_all(as.character)
 
 #Combining Historical and Modern Raptor by Player Datasets
 raptor_by_player <- bind_rows(historical_raptor_by_player,modern_raptor_by_player)
