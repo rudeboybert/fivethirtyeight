eaa8028afe36de72213cee0dc4dbc660f1b4eb03
Jane Bang <jbang19@smith.edu>
2019-11-18 19:45:34 -0500

    Updated nba_draymond data

--- R/data_jane.R
+++ R/data_jane.R
@@ -1,4 +1,4 @@
-#' A Better Way to Evaluate NBA Defense.
+#' A Better Way to Evaluate NBA Defense
 #'
 #' The raw data behind the story
 #' "A Better Way to Evaluate NBA Defense"
@@ -9,7 +9,6 @@
 #'   \item{season}{The second year of the season; for example, 2018-2019 season would be listed as 2019}
 #'   \item{player}{Name of the player}
 #'   \item{possessions}{Number of possessions a player during the season}
-#'   \item{draymond}{Defensive Rating Accounting for Yielding Minimal Openness by Nearest Defender}
-#'    }
-#' @source NBA
+#'   \item{draymond}{Defensive Rating Accounting for Yielding Minimal Openness by Nearest Defender}}
+#' @source see \url{https://github.com/fivethirtyeight/data/tree/master/nba-draymond}
 "nba_draymond"
--- data-raw/process_data_sets_jane.R
+++ data-raw/process_data_sets_jane.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(stringr)
+library(lubridate)
 library(janitor)
 library(usethis)
 
 # nba-draymond ---------------------------------------------------------------
 nba_draymond <- read_csv("data-raw/nba-draymond/draymond.csv") %>%
-  clean_names 
+  clean_names() 
 
 usethis::use_data(nba_draymond, overwrite = TRUE)
