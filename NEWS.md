cbf0bbcebb2b6eeb49da64c81dcf92854339a011
Lizette <lcarpenter@smith.edu>
2019-11-20 20:10:53 -0500

    fixed files and documentation

--- NEWS.md
+++ NEWS.md
@@ -2,6 +2,7 @@
 * Added new data sets:
     + By Natalia Iannucci: `media_mentions_cable`, `media_mentions_online`
     + By Marium Tapal: `state_index`, `state_words`
+    + By Lizette Carpenter: `cabinet_turnover`
 
 
 # fivethirtyeight 0.5.0
--- data-raw/process_data_set_lizette.R
+++ data-raw/process_data_set_lizette.R
@@ -6,9 +6,30 @@ library(usethis)
 
 # cabinet_turnover --------------------------------------------------------------------
 cabinet_turnover <- read_csv("data-raw/cabinet-turnover/cabinet-turnover.csv") %>% 
-  clean_names() %>% 
-  mutate(start = col_date(format = ""),
-         end = col_date(format = ""))
+  clean_names() %>%
+  mutate(combined = str_detect(length, "combined")) %>% 
+  mutate(length = str_replace_all(length, " combined", "")) %>% 
+  mutate(length = as.numeric(length)) %>% 
+  mutate(president = factor(president))  
 
+cabinet_turnover <- cabinet_turnover %>% 
+  mutate(start = replace(start, cabinet_turnover$appointee == "Bob Gates", "12/18/2006" )) %>%
+  mutate(start = replace(start, cabinet_turnover$appointee == "George Tenet", "7/3/95" )) %>% 
+  mutate(start = replace(start, cabinet_turnover$appointee == "Lauro Cavazos", "9/20/88" )) %>% 
+  mutate(start = replace(start, cabinet_turnover$appointee == "Dick Thornburgh", "8/15/88" )) %>%
+  mutate(start = replace(start, cabinet_turnover$appointee == "Bill Webster", "5/26/87" )) %>% 
+  mutate(start = replace(start, cabinet_turnover$appointee == "Nicholas Brady", "9/15/88" )) %>%
+  mutate(end = replace(end, cabinet_turnover$appointee == "Bob Gates", "6/30/2011" )) %>%
+  mutate(end = replace(end, cabinet_turnover$appointee == "George Tenet", "7/11/97" )) %>% 
+  mutate(end = replace(end, cabinet_turnover$appointee == "Lauro Cavazos", "12/12/1990" )) %>% 
+  mutate(end = replace(end, cabinet_turnover$appointee == "Dick Thornburgh", "8/15/1991" )) %>%
+  mutate(end = replace(end, cabinet_turnover$appointee == "Bill Webster", "8/31/1991" )) %>% 
+  mutate(end = replace(end, cabinet_turnover$appointee == "Nicholas Brady", "1/17/1993" ))
+
+cabinet_turnover <- cabinet_turnover %>% 
+  mutate(start = mdy(start)) %>%
+  mutate(end = mdy(end)) 
+
+cabinet_turnover$president <- factor(cabinet_turnover$president, levels = c("Carter", "Reagan", "Bush 41", "Clinton", "Bush 43", "Obama", "Trump"))  
 
 usethis::use_data(cabinet_turnover, overwrite = TRUE)
