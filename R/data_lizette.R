e00bcdf61cf46c82a8f2d83e7955537b7bfbaa2a
Lizette <lcarpenter@smith.edu>
2019-12-09 17:30:50 -0500

    updated data and documentation to cabinet_turnover

--- R/data_lizette.R
+++ R/data_lizette.R
@@ -1,8 +1,8 @@
-#' @title Two Years In, Turnover In Trump’s Cabinet Is Still Historically High
+#' Two Years In, Turnover In Trump’s Cabinet Is Still Historically High
 #' 
-#' @description The raw data behind the story 
+#' The raw data behind the story 
 #' "Two Years In, Turnover In Trump’s Cabinet Is Still Historically High"
-#' \url{https://fivethirtyeight.com/features/two-years-in-turnover-in-trumps-cabinet-is-still-historically-high/}
+#' \url{https://fivethirtyeight.com/features/two-years-in-turnover-in-trumps-cabinet-is-still-historically-high/}.
 #' 
 #' @format A data frame with 312 rows representing cabinet members and 8 variables: 
 #' \describe{
@@ -14,8 +14,6 @@
 #'   \item{length}{Length of Tenure, in days}
 #'   \item{days}{Days into administration that the appointee left office}
 #'   \item{combined}{Whether or not Cabinet member served in more than one administrations}
-#'   }
-#'   
-#' @source Federal Government Websites, News Reports 
+#'   } 
+#' @source from Federal Government Websites and News Reports 
 "cabinet_turnover"
-
--- data-raw/process_data_set_lizette.R
+++ data-raw/process_data_set_lizette.R
@@ -7,29 +7,30 @@ library(usethis)
 # cabinet_turnover --------------------------------------------------------------------
 cabinet_turnover <- read_csv("data-raw/cabinet-turnover/cabinet-turnover.csv") %>% 
   clean_names() %>%
-  mutate(combined = str_detect(length, "combined")) %>% 
-  mutate(length = str_replace_all(length, " combined", "")) %>% 
-  mutate(length = as.numeric(length)) %>% 
-  mutate(president = factor(president))  
-
-cabinet_turnover <- cabinet_turnover %>% 
-  mutate(start = replace(start, cabinet_turnover$appointee == "Bob Gates", "12/18/2006" )) %>%
-  mutate(start = replace(start, cabinet_turnover$appointee == "George Tenet", "7/3/95" )) %>% 
-  mutate(start = replace(start, cabinet_turnover$appointee == "Lauro Cavazos", "9/20/88" )) %>% 
-  mutate(start = replace(start, cabinet_turnover$appointee == "Dick Thornburgh", "8/15/88" )) %>%
-  mutate(start = replace(start, cabinet_turnover$appointee == "Bill Webster", "5/26/87" )) %>% 
-  mutate(start = replace(start, cabinet_turnover$appointee == "Nicholas Brady", "9/15/88" )) %>%
-  mutate(end = replace(end, cabinet_turnover$appointee == "Bob Gates", "6/30/2011" )) %>%
-  mutate(end = replace(end, cabinet_turnover$appointee == "George Tenet", "7/11/97" )) %>% 
-  mutate(end = replace(end, cabinet_turnover$appointee == "Lauro Cavazos", "12/12/1990" )) %>% 
-  mutate(end = replace(end, cabinet_turnover$appointee == "Dick Thornburgh", "8/15/1991" )) %>%
-  mutate(end = replace(end, cabinet_turnover$appointee == "Bill Webster", "8/31/1991" )) %>% 
-  mutate(end = replace(end, cabinet_turnover$appointee == "Nicholas Brady", "1/17/1993" ))
-
-cabinet_turnover <- cabinet_turnover %>% 
-  mutate(start = mdy(start)) %>%
-  mutate(end = mdy(end)) 
-
-cabinet_turnover$president <- factor(cabinet_turnover$president, levels = c("Carter", "Reagan", "Bush 41", "Clinton", "Bush 43", "Obama", "Trump"))  
+  mutate(combined = str_detect(length, "combined"),
+         length = str_replace_all(length, " combined",""),
+         length = as.numeric(length),
+         president = factor(president),
+         start = 
+           case_when(
+             appointee == "Bob Gates" ~ "12/18/2006",
+             appointee == "George Tenet" ~ "7/3/95",
+             appointee == "Lauro Cavazos" ~ "9/20/88",
+             appointee == "Dick Thornburgh" ~ "8/15/88",
+             appointee == "Bill Webster" ~ "5/26/87",
+             appointee == "Nicholas Brady" ~ "9/15/88",
+             TRUE ~ start), 
+         end = 
+           case_when(
+             appointee == "Bob Gates" ~ "6/30/2011",
+             appointee == "George Tenet" ~ "7/11/97",
+             appointee == "Lauro Cavazos" ~ "12/12/1990",
+             appointee == "Dick Thornburgh" ~ "8/15/1991",
+             appointee == "Bill Webster" ~ "8/31/1991",
+             appointee == "Nicholas Brady" ~ "1/17/1993",
+             TRUE ~ end),
+         start = mdy(start),
+         end = mdy(end),
+         president = factor(president, levels = c("Carter", "Reagan", "Bush 41", "Clinton", "Bush 43", "Obama", "Trump")))
 
 usethis::use_data(cabinet_turnover, overwrite = TRUE)
