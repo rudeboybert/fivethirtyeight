1c9bf1af000b2644deddc61def9cd6d509b77520
Fatima <fkeita@smith.edu>
2019-12-09 13:15:06 -0500

    updated data_Fatima.R file, got rid of merge conflicts

--- R/data_fatima.R
+++ R/data_fatima.R
@@ -16,7 +16,6 @@
 #'   \item{text}{poll question}
 #'   \item{category}{category of poll question with 5 categories: impeach and remove, begin proceedings, begin inquiry, reasons, impeach}
 #'   \item{include}{yes/no logical -- value unknown}
-<<<<<<< HEAD
 #'   \item{yes}{Percent of repsondents in sample who answered "Yes" to the poll question}
 #'   \item{no}{Percent of respondents in sample  who answered "No" to the poll question}
 #'   \item{unsure}{Percent of respondents in sample who did not answer "Yes" or "No" to the poll question}
@@ -30,23 +29,7 @@
 #'   \item{ind_yes}{Percent of Independent respondents who answered "yes"}
 #'   \item{ind_no}{Percent of Independent respondents who answered "no"}
 #'   \item{url}{URL links to poll websites}
-#'   \item{notes}{any notes relating to polls in sample
-=======
-#'   \item{yes}{% of repsondents in sample who answered "Yes" to the poll question}
-#'   \item{no}{% of respondents in sample  who answered "No" to the poll question}
-#'   \item{unsure}{% of respondents in sample who did not answer "Yes" or "No" to the poll question}
-#'   \item{rep_sample}{number of Republican respondents in sample}
-#'   \item{rep_yes}{% of Republican respondents who answered "yes"}
-#'   \item{rep_no}{% of Republican respondents who answered "no"}
-#'   \item{dem_sample}{number of Democrat respondents in sample}
-#'   \item{dem_yes}{% of Democrat respondents who answered "yes"}
-#'   \item{dem_no}{% of Democrat respondents who answered "no"}
-#'   \item{ind_sample}{number of Independent respondents in sample}
-#'   \item{ind_yes}{% of Independent respondents who answered "yes"}
-#'   \item{ind_no}{% of Independent respondents who answered "no"}
-#'   \item{url}{URL links to poll websites}
 #'   \item{notes}{any notes relating to polls in sample}
->>>>>>> 25e8defc63e3a707dcecaa074932ec91ea22f474
 #'   }
 #' @source data from \url{https://github.com/fivethirtyeight/data/tree/master/impeachment-polls}.
 "impeachment_polls"
