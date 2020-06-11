b255ad9198930a77f176b1393a584818af97079c
mariumtapal <mariumtapal@gmail.com>
2020-06-11 15:28:25 +0500

    spelling error

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,15 @@
 .pkgenv <- new.env(parent=emptyenv())
 
 .onLoad  <- function(libname, pkgname) {
-  has_data <- requireNamespace("fivethirtyeighdata.test", quietly = TRUE)
+  has_data <- requireNamespace("fivethirtyeightdata.test", quietly = TRUE)
   .pkgenv[["has_data"]] <- has_data
 }
 
 .onAttach <- function(libname, pkgname) {
   if (!.pkgenv$has_data) {
     msg <- paste("To use this package, you must install the",
-                 "fivethirtyeighdata.test package. To install that ",
-                 "package, run `install.packages('fivethirtyeighdata.test',",
+                 "fivethirtyeightdata.test package. To install that ",
+                 "package, run `install.packages('fivethirtyeightdata.test',",
                  "repos='https://mariumtapal.github.io/drat/', type='source')`.")
     msg <- paste(strwrap(msg), collapse="\n")
     packageStartupMessage(msg)
@@ -19,8 +19,8 @@
 hasData <- function(has_data = .pkgenv$has_data) {
   if (!has_data) {
     msg <- paste("To use this function, you must have the",
-                 "`fivethirtyeighdata.test` package installed. See the",
-                 "`fivethirtyeighdata.test` package vignette for more details.")
+                 "`fivethirtyeightdata.test` package installed. See the",
+                 "`fivethirtyeightdata.test` package vignette for more details.")
     msg <- paste(strwrap(msg), collapse="\n")
     stop(msg)
   }
