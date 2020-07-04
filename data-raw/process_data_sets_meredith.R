f0472771f6df899f7532b398fa5fc988cf441672
mariumtapal <mariumtapal@gmail.com>
2020-07-04 15:33:47 +0500

    transfer mlb_elo to 538data

--- data-raw/process_data_sets_meredith.R
+++ data-raw/process_data_sets_meredith.R
@@ -96,18 +96,6 @@ usethis::use_data(generic_topline, overwrite = TRUE)
 
 
 
-# mlb-elo ---------------------------------------------------------------------------
-mlb_elo <- read_csv("https://projects.fivethirtyeight.com/mlb-api/mlb_elo.csv") %>%
-  mutate(
-    playoff = as.factor(playoff),
-    playoff = ifelse(playoff == "<NA>", NA, playoff),
-    neutral = as.logical(neutral)
-  ) %>%
-  slice(1:100)
-usethis::use_data(mlb_elo, overwrite = TRUE)
-
-
-
 # soccer-spi ------------------------------------------------------------------------
 # spi_matches
 spi_matches <- 
