965fb99eca21d5098f2f1d2319c6b09c0280f0d2
mariumtapal <mariumtapal@gmail.com>
2020-07-15 18:01:36 +0500

    move a bunch of datasets over to 538data

--- R/data_maggie.R
+++ R/data_maggie.R
@@ -477,29 +477,6 @@
 
 
 
-#' The Worst Tweeter in Politics Isn't Trump
-#'
-#' The raw data behind:
-#' "The Worst Tweeter in Politics Isn't Trump"
-#' \url{https://fivethirtyeight.com/features/the-worst-tweeter-in-politics-isnt-trump/}
-#'
-#' Presidents Dataset:
-#' @format A data frame with 6439 rows describing individual tweets and 8 variables:
-#' \describe{
-#' \item{created_at}{The date and time the tweet was posted}
-#' \item{user}{The user posting the tweet}
-#' \item{text}{The text of the tweet}
-#' \item{url}{The link to the tweet}
-#' \item{replies}{The number of replies to the tweet}
-#' \item{retweets}{The number of retweets}
-#' \item{favorites}{The number of favorites}
-#' }
-#' @details Data on President Obama's tweets collected on Oct 20, President Trump's tweets collected on Oct 23.
-#' @source Twitter \url{https://twitter.com/BarackObama} and \url{https://twitter.com/realDonaldTrump}
-"twitter_presidents"
-
-
-
 #' Mayweather Is Defined By The Zero Next To His Name
 #'
 #' The raw data behind:
--- data-raw/process_data_set_jessica.R
+++ data-raw/process_data_set_jessica.R
@@ -1,18 +1,18 @@
-library(tidyverse)
-library(stringr)
-library(lubridate)
-library(janitor)
-library(usethis)
-
-nba_all_elo <- read_csv("data-raw/nba-elo/nbaallelo.csv")%>%
-  arrange(game_id, `_iscopy`)%>%
-  mutate_if(is.character,as.factor)%>%
-  mutate(opp_win_equiv = lead(win_equiv), 
-         opp_seasongame = lead(seasongame, 1),
-         date_game = mdy(date_game), 
-         is_playoffs = as.logical(is_playoffs),
-         notes =  as.character(notes))%>%
-  filter(`_iscopy` == 0)%>%
-  select(-c(`_iscopy`, game_location))
-
-usethis::use_data(nba_all_elo, overwrite = TRUE)
+# library(tidyverse)
+# library(stringr)
+# library(lubridate)
+# library(janitor)
+# library(usethis)
+# 
+# nba_all_elo <- read_csv("data-raw/nba-elo/nbaallelo.csv")%>%
+#   arrange(game_id, `_iscopy`)%>%
+#   mutate_if(is.character,as.factor)%>%
+#   mutate(opp_win_equiv = lead(win_equiv), 
+#          opp_seasongame = lead(seasongame, 1),
+#          date_game = mdy(date_game), 
+#          is_playoffs = as.logical(is_playoffs),
+#          notes =  as.character(notes))%>%
+#   filter(`_iscopy` == 0)%>%
+#   select(-c(`_iscopy`, game_location))
+# 
+# usethis::use_data(nba_all_elo, overwrite = TRUE)
