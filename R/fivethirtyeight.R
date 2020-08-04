f5de8af6ecc75a13d33b7e7325ec2c58f842ebc9
rudeboybert <albert.ys.kim@gmail.com>
2020-08-04 11:54:34 -0400

    More fedora-clang-devel error debugging

--- R/fivethirtyeight.R
+++ R/fivethirtyeight.R
@@ -1,10 +1,10 @@
 #' fivethirtyeight: Data and Code Behind the Stories and Interactives at 'FiveThirtyEight'
 #'
-#' An R package that provides access to the code and data sets
-#' published by FiveThirtyEight \url{https://github.com/fivethirtyeight/data}. Note
-#' that while we received guidance from editors at 538, this package is not
-#' officially published by 538. Contribute to this package at
-#' \url{https://github.com/rudeboybert/fivethirtyeight}.
+#' An R package that provides access to the code and data sets published by
+#' FiveThirtyEight \url{https://github.com/fivethirtyeight/data}. Note that
+#' while we received guidance from editors at 538, this package is not
+#' officially published by 538. You can explore all datasets here:
+#' \url{https://fivethirtyeight-r.netlify.app/articles/fivethirtyeight.html}
 #'
 #' @docType package
 #' @name fivethirtyeight
@@ -18,7 +18,4 @@
 #'
 #' # To view a list of all data sets:
 #' \donttest{data(package = "fivethirtyeight")}
-#'
-#' # To view a detailed list of all data sets:
-#' \donttest{vignette("fivethirtyeight", package = "fivethirtyeight")}
 NULL
